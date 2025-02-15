468c4cfa1ff7de2ace343b09a96735c9814321dd
parth007d <parthsagar1694@gmail.com>
2025-02-15 11:22:26 +0530

    Third

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,9 @@
-import React, { useState } from "react";
+import React from 'react'
 
 const App = () => {
-  const [formdata, setformdata] = useState({
-    first: "",
-    second: "",
-  });
-  const [result, setresult] = useState(0);
-//new commit
-  const ChangeHadler = (e) => {
-    setformdata({
-      ...formdata,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const sawvalue = () => {
-    setresult(Number(formdata.first) + Number(formdata.second));
-  };
-
   return (
-    <>
-      <input
-        type="number"
-        placeholder="value"
-        name="first"
-        onChange={ChangeHadler}
-        value={formdata.first}
-      />
-      <input
-        type="number"
-        placeholder="value"
-        name="second"
-        onChange={ChangeHadler}
-        value={formdata.second}
-      />
-      <button onClick={sawvalue}>Add</button>
-      <h1>{result}</h1>
-    </>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
